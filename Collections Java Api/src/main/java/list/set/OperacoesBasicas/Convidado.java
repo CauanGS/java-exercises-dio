e367ff4da1911b2cc2ac282fd1c31db0515d5039
cauan <cauangabrieldesouza@gmail.com>
2025-03-12 20:27:42 -0300

    Resolvendo o exercicio de Operacoes Basicas utilizando set

---
+++ Collections Java Api/src/main/java/list/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,35 @@
+package main.java.list.set.OperacoesBasicas;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite) {
+        this.codigoConvite = codigoConvite;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    public void setCodigoConvite(int codigoConvite) {
+        this.codigoConvite = codigoConvite;
+    }
+
+    @Override
+    public String  toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
