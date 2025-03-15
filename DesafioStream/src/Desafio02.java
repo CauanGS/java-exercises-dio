9ea55531d2ea783f86d8e53504080cbf2e9e0987
cauan <cauangabrieldesouza@gmail.com>
2025-03-15 18:28:20 -0300

    Resolvendo o desafio 02

--- DesafioStream/src/Desafio02.java
+++ DesafioStream/src/Desafio02.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Desafio02 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        // Desafio
+        // Desafio - Imprima a soma dos números pares da lista:
+        numeros.stream().filter(numero -> numero % 2 == 0).forEach(System.out::println);
     }
 }
