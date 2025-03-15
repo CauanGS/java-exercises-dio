46f807c26730ab1c30e4a696e57c3375db7e1a16
cauan <cauangabrieldesouza@gmail.com>
2025-03-15 18:35:54 -0300

    Resolvendo o desafio 04

--- DesafioStream/src/Desafio04.java
+++ DesafioStream/src/Desafio04.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Desafio04 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        // Desafio
+        // Desafio 4 - Remova todos os valores ímpares:
+        numeros.stream().filter(num -> num % 2 == 0).forEach(System.out::println);
     }
 }
