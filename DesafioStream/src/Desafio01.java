0749c3ed0d0663a6ee36d3e13cde62e0da34f6a1
cauan <cauangabrieldesouza@gmail.com>
2025-03-15 18:21:52 -0300

    Corrigindo o desafio 01

--- DesafioStream/src/Desafio01.java
+++ DesafioStream/src/Desafio01.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Desafio01 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
         // Desafio 1 - Mostre a lista na ordem numérica:
-        numeros.sort((n1, n2) -> Integer.compare(n1, n2));
-        System.out.println(numeros);
+        List<Integer> numerosOrdenados = numeros.stream().sorted().collect(Collectors.toList());
+        System.out.println(numerosOrdenados);
     }
 
 }
