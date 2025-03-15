1ebde8b9faf1f3948478b0bd7379e415fe4aa0c3
cauan <cauangabrieldesouza@gmail.com>
2025-03-15 18:34:19 -0300

    Resolvendo o desafio 03

--- DesafioStream/src/Desafio02.java
+++ DesafioStream/src/Desafio02.java
@@ -5,7 +5,7 @@ import java.util.function.Consumer;
 public class Desafio02 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        // Desafio - Imprima a soma dos números pares da lista:
+        // Desafio 02 - Imprima a soma dos números pares da lista:
         numeros.stream().filter(numero -> numero % 2 == 0).forEach(System.out::println);
     }
 }
---
+++ DesafioStream/src/Desafio03.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio03 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        // Desafio 03 - Verifique se todos os números da lista são positivos:
+        System.out.println(numeros.stream().allMatch(numero -> numero >=0));
+    }
+}
